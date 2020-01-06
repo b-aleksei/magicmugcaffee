691edf555898345af02c5b44ae291d66592ae96e
Aleksei Bugaev <103301@mail.ru>
2020-01-06 17:16:25 +0300

    work contact

--- source/js/price.js
+++ source/js/price.js
@@ -1,15 +1,17 @@
-let select = document.querySelector('.sel'),
+let select = document.querySelector('.price__select'),
   col2 = document.querySelectorAll('.price__cell--2'),
   col3 = document.querySelectorAll('.price__cell--3'),
   col4 = document.querySelectorAll('.price__cell--4'),
-  price = document.querySelector('.price');
+  noJs = document.querySelector('.price__no-js');
+
+noJs.classList.remove("price__no-js");
 
 select.onchange = function() {
   let indexSelected = select.selectedIndex,
     option = select.querySelectorAll('option')[indexSelected],
     selectedId = option.getAttribute('id');
 
-  if( selectedId === '2' ) {
+  if( selectedId === 'col-2' ) {
     for(let i = 0; i < col2.length; i++) {
       col2[i].classList.remove("hide");
     }
@@ -18,7 +20,7 @@ select.onchange = function() {
       col2[i].classList.add("hide");
     }
   }
-  if( selectedId === '3' ) {
+  if( selectedId === 'col-3' ) {
     for(let i = 0; i < col2.length; i++) {
       col2[i].classList.add("hide");
       col3[i].classList.remove("hide");
@@ -28,7 +30,7 @@ select.onchange = function() {
       col3[i].classList.add("hide");
     }
   }
-  if( selectedId === '4' ) {
+  if( selectedId === 'col-4' ) {
     for(let i = 0; i < col2.length; i++) {
       col2[i].classList.add("hide");
       col4[i].classList.remove("hide");
