2e2ce232f4dfcd724d55e72329ce9fe229dafa38
Aleksei Bugaev <103301@mail.ru>
2020-01-18 12:06:27 +0300

    create nav-menu without pos. absolute

--- source/js/menu-open.js
+++ source/js/menu-open.js
@@ -3,8 +3,11 @@
 
   let navButton = document.querySelector(".nav__menu-opener");
   let navMain = document.querySelector(".nav");
+
+  navButton.classList.remove("hide");
   navMain.classList.remove("header__nav--no-js");
   navButton.addEventListener("click", function () {
     navMain.classList.toggle("header__nav--menu-open");
+    navButton.classList.toggle("header__nav--menu-open");
   });
 })();
