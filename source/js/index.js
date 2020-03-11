7149247710a7c9b359c8b3158fa8209c6d0174d8
Aleksei Bugaev <103301@mail.ru>
2020-03-11 22:04:44 +0300

    before correct sprite

--- source/js/index.js
+++ source/js/index.js
@@ -10,13 +10,3 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-/*
-if ('serviceWorker' in navigator) {
-  // Весь код регистрации у нас асинхронный.
-  navigator.serviceWorker.register('./sw.js')
-    .then(() => navigator.serviceWorker.ready.then((worker) => {
-      worker.sync.register('syncdata');
-    }))
-    .catch((err) => console.log(err));
-}
-*/
