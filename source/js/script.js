0360cfd20fb3b22239af780abccb5509f5088edf
Aleksei Bugaev <103301@mail.ru>
2020-03-21 19:13:55 +0300

    refactor script.js

--- source/js/script.js
+++ source/js/script.js
@@ -30,7 +30,7 @@
   function backToTop() {
     if (window.pageYOffset > 0) {
       window.scrollBy(0, -80);
-      setTimeout(backToTop, 0);
+      setTimeout(backToTop);
     }
   }
 
@@ -55,47 +55,30 @@
 })();
 //=====================price===========================================
 (function () {
-  let select = document.querySelector(".price__select"),
-    col2 = document.querySelectorAll(".price__cell--2"),
-    col3 = document.querySelectorAll(".price__cell--3"),
-    col4 = document.querySelectorAll(".price__cell--4"),
-    noJs = document.querySelector(".price__no-js");
+    let table = document.querySelector('.price');
+  let select = table.querySelector(".price__select");
+   let noJs = table.querySelector(".price__no-js");
 
   noJs.classList.remove("price__no-js");
-  select.onchange = function () {
-    let indexSelected = select.selectedIndex,
-      option = select.querySelectorAll("option")[indexSelected],
-      selectedId = option.getAttribute("id");
-    if (selectedId === "col-2") {
-      for (let i = 0; i < col2.length; i++) {
-        col2[i].classList.remove("hide");
-      }
-    } else {
-      for (let i = 0; i < col2.length; i++) {
-        col2[i].classList.add("hide");
-      }
+
+  select.addEventListener('change', function () {
+    console.log(this);
+    let dictionary = {
+      2 : [".price__cell--3", ".price__cell--4"],
+      3 : [".price__cell--2", ".price__cell--4"],
+      4 : [".price__cell--2", ".price__cell--3"],
+    };
+    let showColumns = table.querySelectorAll(".price__cell--" + this.value);
+    let hideColumns1 = table.querySelectorAll(dictionary[this.value][0]);
+    let hideColumns2 = table.querySelectorAll(dictionary[this.value][1]);
+
+    for (let i = 0; i < showColumns.length; i++) {
+      showColumns[i].classList.remove("hide");
+      hideColumns1[i].classList.add("hide");
+      hideColumns2[i].classList.add("hide");
     }
-    if (selectedId === "col-3") {
-      for (let i = 0; i < col2.length; i++) {
-        col2[i].classList.add("hide");
-        col3[i].classList.remove("hide");
-      }
-    } else {
-      for (let i = 0; i < col2.length; i++) {
-        col3[i].classList.add("hide");
-      }
-    }
-    if (selectedId === "col-4") {
-      for (let i = 0; i < col2.length; i++) {
-        col2[i].classList.add("hide");
-        col4[i].classList.remove("hide");
-      }
-    } else {
-      for (let i = 0; i < col2.length; i++) {
-        col4[i].classList.add("hide");
-      }
-    }
-  };
+  })
+
 })();
 
 //=====================slider===========================================
@@ -103,7 +86,7 @@
   let slider = document.querySelector('.slider'),
     buttonForward = slider.querySelector(".slider__arrow-forward"),
     buttonBack = slider.querySelector(".slider__arrow-back"),
-    slideContainer = document.querySelector('.slider__slides'),
+    slideContainer = slider.querySelector('.slider__slides'),
     amountSlides = slider.querySelectorAll('.slider__item').length,
     indicatorContainer = slider.querySelector('.slider__indicators'),
     translate = 0,
@@ -131,9 +114,9 @@
   let onClickSlider = function () {
     clearTimeout(timer);
 
-      clearTimeout(delaySlide);
-      clearInterval(intervalSlider);
-      slideContainer.classList.add('click-duration');
+    clearTimeout(delaySlide);
+    clearInterval(intervalSlider);
+    slideContainer.classList.add('click-duration');
     slideContainer.classList.remove('auto-duration');
     let forward = this === buttonForward;
     indicatorContainer.children[translate].style.backgroundColor = '';
@@ -182,8 +165,25 @@
 //=================validation form======================================
 (function () {
 
+  const START_INDEX = 4;
+  const CLOSE_BRACE = 6;
+  let form = document.getElementById('form');
+  let name = document.getElementById('name');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+  let startSelection = 0;
+  let endSelection = 0;
+  let pastePattern = ['+', '9', ' ', '(', '9', '9', '9', ')', ' ', '9', '9', '9', '-', '9', '9', '-', '9', '9'];
+  let pattern = ['+', '1', ' ', '(', '_', '_', '_', ')', ' ', '_', '_', '_', '-', '_', '_', '-', '_', '_'];
+  let result = pattern.slice();
+  let focus;
+  let initialValue = pattern.join('');
+  let successSend = document.querySelector('.success-send');
+  let btnText = form.querySelector('.contact__btn-name');
+  let preloader = form.querySelector('.sk-three-bounce');
+
   let checkValidity = function () {
-    if(this.validity.patternMismatch || this.value === '') {
+    if (this.validity.patternMismatch || this.value === '') {
       this.style.borderColor = '#ba0b11';
       this.parentElement.classList.remove('valid')
     } else {
@@ -192,18 +192,7 @@
     }
   };
 
-  const START_INDEX = 4;
-  const CLOSE_BRACE = 6;
-  let phone = document.getElementById('phone');
-  // phone.maxLength = 18;
-  let startSelection = 0;
-  let endSelection = 0;
-  let pastePattern = ['+', '9', ' ', '(', '9', '9', '9', ')', ' ', '9', '9', '9', '-', '9', '9', '-', '9', '9'];
-  let pattern = ['+', '1', ' ', '(', '_', '_', '_', ')', ' ', '_', '_', '_', '-', '_', '_', '-', '_', '_'];
-  let result = pattern.slice();
-  let focus = START_INDEX;
-
-  phone.addEventListener('paste', function () {
+  let pasteValue = function () {
     setTimeout(() => {
       let value = Array.from(this.value).filter(item => /\d/.test(item));
       value.reverse();
@@ -218,14 +207,14 @@
       startSelection = endSelection = 0;
       checkValidity.call(this);
     })
-  });
+  };
 
-  phone.addEventListener('select', function () {
+  let selectValue = function () {
     startSelection = this.selectionStart;
     endSelection = this.selectionEnd;
-  });
+  };
 
-  phone.addEventListener('keydown', function (e) {
+  let enterValue = function (e) {
 
     let IsSelectionTrue = startSelection !== endSelection;
     if (!e.ctrlKey) {
@@ -294,23 +283,8 @@
       }
     }
     checkValidity.call(this);
-  });
-
-  let name = document.getElementById('name');
-  name.addEventListener('input', checkValidity);
-
-  let email = document.getElementById('email');
-  email.addEventListener('input', checkValidity);
-
-})();
-//=====================send===========================================
-(function () {
-
-  let form = document.getElementById('form');
-  let successSend = document.querySelector('.success-send');
-  let btnText = form.querySelector('.contact__btn-name');
-  let preloader = form.querySelector('.sk-three-bounce');
-
+  };
+  // ============================== send form================================
   let showPreload = function () {
     btnText.textContent = 'SENDING';
     btnText.classList.add('sending');
@@ -319,13 +293,12 @@
 
   let closePopup = function (evt) {
     if (evt.code === "Escape") {
-    successSend.classList.add("display-none");
-    document.removeEventListener("keydown", closePopup);
+      successSend.classList.add("display-none");
+      document.removeEventListener("keydown", closePopup);
     }
   };
 
   let sendSuccess = function () {
-
     successSend.classList.remove("display-none");
     btnText.textContent = 'SEND';
     btnText.classList.remove('sending');
@@ -337,10 +310,10 @@
     }, {once: true});
 
     let inputsWraps = form.querySelectorAll('label:not(:last-of-type)');
-    inputsWraps.forEach((item) => item.classList.remove('valid'));
+    inputsWraps.forEach(item => item.classList.remove('valid'));
   };
 
-  form.addEventListener('submit', function (e) {
+   let onSubmit = function (e) {
     showPreload();
     fetch('mail.php', {
       method: 'post',
@@ -350,6 +323,47 @@
       form.reset()
     }).catch(() => alert('There are network problems. Send the application again'));
     e.preventDefault()
-  })
+  };
+
+  form.addEventListener('focusin', function (e) {
+    this.addEventListener('submit', onSubmit);
+
+    if (e.target === phone) {
+      phone.value = initialValue || phone.value;
+      setTimeout(() => {
+        phone.selectionStart = phone.selectionEnd = focus || START_INDEX;
+      });
+      initialValue = '';
+      phone.addEventListener('paste', pasteValue);
+      phone.addEventListener('select', selectValue);
+      phone.addEventListener('keydown', enterValue);
+    }
+
+    if (e.target === name) {
+      name.addEventListener('input', checkValidity);
+    }
+
+    if (e.target === email) {
+      email.addEventListener('input', checkValidity);
+    }
+  });
+
+  form.addEventListener('focusout', function (e) {
+    this.removeEventListener('submit', onSubmit);
+
+    if (e.target === phone) {
+    phone.removeEventListener('paste', pasteValue);
+    phone.removeEventListener('select', selectValue);
+    phone.removeEventListener('keydown', enterValue);
+    }
+
+    if (e.target === name) {
+      name.removeEventListener('input', checkValidity);
+    }
+
+    if (e.target === email) {
+      email.removeEventListener('input', checkValidity);
+    }
+  });
 
 })();
