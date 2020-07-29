1c5eef3acaac69d52d92ee785b75697f83de3ff2
Aleksei Bugaev <103301@mail.ru>
2020-07-29 20:03:34 +0300

    test2

--- src/js/sw.register.js
+++ src/js/sw.register.js
@@ -1,13 +1,14 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js', {updateViaCache: 'none'}).then(registration => {
-			if (registration.waiting) {
+		navigator.serviceWorker.register('/service-worker.js', {updateViaCache: 'none'})
+			.then(registration => {
+				if (registration.waiting) {
 				// Показываем приглашение обновить страницу
-				console.log('Обновите страницу');
-			}
-			console.log('ServiceWorker registration successful');
-		}).catch(err => {
-			console.log('ServiceWorker registration failed: ', err);
-		});
+					console.log('Обновите страницу');
+				}
+				console.log('ServiceWorker registration successful');
+			}).catch(err => {
+				console.log('ServiceWorker registration failed: ', err);
+			});
 	});
 }
