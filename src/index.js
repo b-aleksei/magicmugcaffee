3b10268b41ab28b2b35ec428a1292f484e4c3efc
Aleksei Bugaev <103301@mail.ru>
2020-07-30 10:44:03 +0300

    remove sw

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import './scss/style.scss';
 import './js/script';
-import './js/sw.register';
-
-
-/* if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js')
-			.then(() => {
-				console.log('ServiceWorker registration successful');
-			}).catch(err => {
-				console.log('ServiceWorker registration failed: ', err);
-			});
-	});
-}*/
+// import './js/sw.register';
